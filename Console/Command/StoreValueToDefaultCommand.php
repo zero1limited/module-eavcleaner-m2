9d34c9d2168be235355e012f7e852877884ea64e
adamzero1 <adam.crossland@zero1.co.uk>
2024-01-03 14:43:51 +0000

    feat: ability to copy store level value to default

---
+++ Console/Command/StoreValueToDefaultCommand.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Hackathon\EAVCleaner\Console\Command;
+
+use Magento\Framework\App\ProductMetadataInterface;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Model\ResourceModel\IteratorFactory;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+class StoreValueToDefaultCommand extends Command
+{
+    /** @var IteratorFactory */
+    protected $iteratorFactory;
+
+    /**
+     * @var ProductMetaDataInterface
+     */
+    protected $productMetaData;
+
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
+    public function __construct(
+        IteratorFactory $iteratorFactory,
+        ProductMetaDataInterface $productMetaData,
+        ResourceConnection $resourceConnection,
+        string $name = null
+    ) {
+        parent::__construct($name);
+
+        $this->iteratorFactory = $iteratorFactory;
+        $this->productMetaData    = $productMetaData;
+        $this->resourceConnection = $resourceConnection;
+    }
+
+    protected function configure()
+    {
+        $description = "Set global value for product to that of the value at store view.";
+        $this
+            ->setName('eav:attributes:store-value-to-default')
+            ->setDescription($description)
+            ->addOption('dry-run')
+            ->addOption('force')
+            ->addOption(
+                'store-id',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Store to run on',
+                null
+            )
+            ->addOption(
+                'attribute-id',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Attribute ID to clean up',
+                null
+            )
+            ->addOption(
+                'entity',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Set entity to cleanup (product or category)',
+                'product'
+            );
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $isDryRun = $input->getOption('dry-run');
+        $isForce  = $input->getOption('force');
+        $storeId   = $input->getOption('store-id');
+        $attributeId   = $input->getOption('attribute-id');
+        $entity   = $input->getOption('entity');
+
+        if (!in_array($entity, ['product', 'category'])) {
+            $output->writeln('Please specify the entity with --entity. Possible options are product or category');
+
+            return 1; // error.
+        }
+
+        if (!$isDryRun && !$isForce) {
+            if (!$input->isInteractive()) {
+                $output->writeln('ERROR: neither --dry-run nor --force options were supplied, and we are not running interactively.');
+
+                return 1; // error.
+            }
+
+            $output->writeln('WARNING: this is not a dry run. If you want to do a dry-run, add --dry-run.');
+            $question = new ConfirmationQuestion('Are you sure you want to continue? [No] ', false);
+
+            if (!$this->getHelper('question')->ask($input, $output, $question)) {
+                return 1; // error.
+            }
+        }
+
+        if($storeId === 0 || $storeId === '0') {
+            $output->writeln('Cannot run on default level!');
+            return 1;
+        }
+
+        if(!$storeId){
+            $output->writeln('You must specify a store ID');
+            return 1;
+        }
+
+        if(!$attributeId){
+            $output->writeln('You must specify an attribute ID');
+            return 1;
+        }
+
+        $dbRead = $this->resourceConnection->getConnection('core_read');
+        $dbWrite = $this->resourceConnection->getConnection('core_write');
+        $counts = [];
+        $tables = ['varchar', 'int', 'decimal', 'text', 'datetime'];
+        $column = $this->productMetaData->getEdition() === 'Enterprise' ? 'row_id' : 'entity_id';
+
+        foreach ($tables as $table) {
+            // Select all non-global values
+            $fullTableName = $this->resourceConnection->getTableName('catalog_' . $entity . '_entity_' . $table);
+
+            // NULL values are handled separately
+
+            $sql = 'SELECT * FROM '.$fullTableName.' WHERE store_id != 0';
+            $sql .= ' AND store_id = '.$storeId;
+            $sql .= ' AND attribute_id = '.$attributeId;
+            $sql .= ' AND value IS NOT NULL';
+
+            $query = $dbRead->query($sql);
+
+            $iterator = $this->iteratorFactory->create();
+            $iterator->walk($query, [function (array $result) use ($column, &$counts, $dbRead, $dbWrite, $fullTableName, $isDryRun, $output): void {
+                $row = $result['row'];
+                $output->writeln('updating entity '.$row['entity_id'].' value: '.$row['value']);
+                if(!$isDryRun){
+                    $dbWrite->query(
+                        'INSERT INTO '.$fullTableName.' ('.$column.', attribute_id, store_id, value) values (?, ?, ?, ?) ON DUPLICATE KEY UPDATE value = ?',
+                        [
+                            $row['entity_id'],
+                            $row['attribute_id'],
+                            0,
+                            $row['value'],
+                            $row['value']
+                        ]
+                    );
+                }
+            }]);
+        }
+
+        return 0; // success.
+    }
+}
